28024317c7cb314cd3bc3f7b4f2978305bf81f88
johnlagoutaris <jjlagoutaris@gmail.com>
2025-12-24 12:11:41 -0500

    Updated for Christmas 2025

--- index.js
+++ index.js
@@ -1,272 +1,280 @@
 let round = 0;
-let answers = ['sponge', 'all', 'towel', 'age', 'darkness', 
-'future', 'promise', 'bald', 'hidden', 'technology', 'magic', 'incorrectly'];
+let answers = ['front seat', '595', 'trash', 'glovebox'];
 let outcomeResult = 'incorrect';
 
 function clearScreenAndLoadRiddle() {
-  if(round == 8){
-    document.body.innerHTML = `<!-- So you've found me... Well done! 
-  
-    Welcome to the inside of the internet... kind of :)
-  
-    You can do all kinds of stuff back here.
-  
-    For example, you can make websites for riddles :P
-  
-    Anyway, let me not get distracted. Your answer is, uhhh - HIDDEN - in plain sight :)
-  -->`;
-    loadRiddle();
-  } else if (round == 9) {
-    document.body.innerHTML = `  <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <!-- 
-    *BURIED TREASURE*
+
+    if(round == 1000){
+      // if(round == 8){
+    //   document.body.innerHTML = `<!-- So you've found me... Well done! 
     
-    Oh no, now everything's so... white. If only we could change the background-color to something a little darker, like, I don't know, say, black? Or brown? Or any color you can think of but white! :D
+    //   Welcome to the inside of the internet... kind of :)
+    
+    //   You can do all kinds of stuff back here.
+    
+    //   For example, you can make websites for riddles :P
+    
+    //   Anyway, let me not get distracted. Your answer is, uhhh - HIDDEN - in plain sight :)
+    // -->`;
+    //   loadRiddle();
+    // } else if (round == 9) {
+    //   document.body.innerHTML = `  <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <!-- 
+    //   *BURIED TREASURE*
+      
+    //   Oh no, now everything's so... white. If only we could change the background-color to something a little darker, like, I don't know, say, black? Or brown? Or any color you can think of but white! :D
 
-    Open my container of riddles if you dare (sometimes the buried treasure is found beneath the buried treasure). Inside you'll find some things that are... out of style and desperately in need of some color!
-  -->
-    `
-    loadRiddle();
-    console.log(document.body.children[100].children[0]);
-    document.body.style = `background-image: none`;
-    document.body.children[100].children[0].textContent = `TECHNOLOGY can be wonderful :)`
-    document.body.children[100].children[0].style.backgroundColor = 'white';
-    document.body.children[100].children[0].style.color = 'FIX ME';
-    document.body.children[100].children[1].style.border = `2px solid white`;
-    document.body.children[100].children[1].style.color = `FIX ME`;
-    document.body.children[100].children[2].style.border = `2px solid white`;
-    document.body.children[100].children[2].style.color = `FIX ME`;
-    document.body.children[100].children[2].style.backgroundColor = `white`;
-  } else if (round == 10) {
-    document.body.innerHTML = `<p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-    <p></p>
-  
-    <!-- *BURIED TREASURE #2*
-       
-    Drat. What happened now? It's awfully dark in here, isn't it? Why don't we try turning the lights back on? Try boosting the opacity up a notch to see things for what they are.
+    //   Open my container of riddles if you dare (sometimes the buried treasure is found beneath the buried treasure). Inside you'll find some things that are... out of style and desperately in need of some color!
+    // -->
+    //   `
+    //   loadRiddle();
+    //   console.log(document.body.children[100].children[0]);
+    //   document.body.style = `background-image: none`;
+    //   document.body.children[100].children[0].textContent = `TECHNOLOGY can be wonderful :)`
+    //   document.body.children[100].children[0].style.backgroundColor = 'white';
+    //   document.body.children[100].children[0].style.color = 'FIX ME';
+    //   document.body.children[100].children[1].style.border = `2px solid white`;
+    //   document.body.children[100].children[1].style.color = `FIX ME`;
+    //   document.body.children[100].children[2].style.border = `2px solid white`;
+    //   document.body.children[100].children[2].style.color = `FIX ME`;
+    //   document.body.children[100].children[2].style.backgroundColor = `white`;
+    // } else if (round == 10) {
+    //   document.body.innerHTML = `<p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    //   <p></p>
+    
+    //   <!-- *BURIED TREASURE #2*
+        
+    //   Drat. What happened now? It's awfully dark in here, isn't it? Why don't we try turning the lights back on? Try boosting the opacity up a notch to see things for what they are.
 
-    -->`
-    loadRiddle();
-    document.body.style = `background-image: none; background-color: black`;
-    document.body.children[100].children[0].innerHTML = `but so is <span style='opacity: 0;'>MAGIC</span>`;
-    document.body.children[100].children[0].style.backgroundColor = 'white';
-    document.body.children[100].children[0].style.color = 'black';
-    document.body.children[100].children[1].style.border = `2px solid transparent`;
-    document.body.children[100].children[1].style.color = `black`;
-    document.body.children[100].children[2].style.border = `2px solid transparent`;
-    document.body.children[100].children[2].style.color = `black`;
-    document.body.children[100].children[2].style.backgroundColor = `greenyellow`;
-  } else if (round == 11) {
-    document.body.innerHTML = ``;
-    loadRiddle();
-    document.body.style = `background: url(./imgs/bg.jpg) center / cover no-repeat; background-color: white`;
-  } else if (round == 12){
-    document.body.innerHTML = `
-    <!-- Good job! Thanks for playing! :) - John -->`;
-    document.body.style = `background: url(./imgs/end.gif) center / cover no-repeat; background-color: white`;
-  } else{
-    document.body.innerHTML = ``;
-    loadRiddle();
-    document.body.style = `background: url(./imgs/bg.jpg) center / cover no-repeat; background-color: white`;
-  }
-  
+    //   -->`
+    //   loadRiddle();
+    //   document.body.style = `background-image: none; background-color: black`;
+    //   document.body.children[100].children[0].innerHTML = `but so is <span style='opacity: 0;'>MAGIC</span>`;
+    //   document.body.children[100].children[0].style.backgroundColor = 'white';
+    //   document.body.children[100].children[0].style.color = 'black';
+    //   document.body.children[100].children[1].style.border = `2px solid transparent`;
+    //   document.body.children[100].children[1].style.color = `black`;
+    //   document.body.children[100].children[2].style.border = `2px solid transparent`;
+    //   document.body.children[100].children[2].style.color = `black`;
+    //   document.body.children[100].children[2].style.backgroundColor = `greenyellow`;
+    // } else if (round == 11) {
+    //   document.body.innerHTML = ``;
+    //   loadRiddle();
+    //   document.body.style = `background: url(./imgs/ChristmasGameshow.jpg) center / cover no-repeat; background-color: white`;
+    // } else if (round == 12){
+    //   document.body.innerHTML = `
+    //   <!-- Good job! Thanks for playing! :) - John -->`;
+    //   document.body.style = `background: url(./imgs/end.gif) center / cover no-repeat; background-color: white`;
+    // } 
+
+    }
+    else if (round == 4){
+      document.body.innerHTML = `That is the end of the website portion of tonight's game! See you next year!`;
+      document.body.style = `background: url(./imgs/end.gif) center / cover no-repeat; background-color: white`;
+    } 
+
+    else{
+      document.body.innerHTML = ``;
+      loadRiddle();
+      document.body.style = `background: url(./imgs/ChristmasGameshow.jpg) center / cover no-repeat; background-color: white`;
+    }
 }
 
 let startBtn = document.querySelector('.startBtn');
@@ -293,7 +301,7 @@ const createHelpModal = () => {
   help.style = `font-size: 24px`;
   help.innerHTML = `
   <li>Answers are case insensitive, so capitalization is not important.</li>
-  <li>All answers should be just one word. Omit articles (i.e. 'a', 'an', 'the').</li>
+  <li>Omit articles (i.e. 'a', 'an', 'the').</li>
   <br><hr><br>Example: What is the opposite of a lie?<br><br><span style='color: green'>Valid:</span> truTH<br><br><span style="color: red">Invalid:</span> The Truth<br><br><hr><br>May the odds be ever in your favor. :)`;
 
   const continueBtn = document.createElement('button');
@@ -363,7 +371,23 @@ function loadRiddle() {
 
   let title = document.createElement('h2');
   title.classList.add('title');
-  title.textContent = `QUESTION ${round + 1}`;
+  switch(round){
+    case 0:
+      title.textContent = `QUESTION ${round + 1}: Where Michael sits to not trigger his vertigo... `;
+      break;
+    case 1:
+      title.textContent = `QUESTION ${round + 1} - After you have completed the individual challenges to your satisfaction, enter the group number here: `;
+      break;
+    case 2:
+      title.textContent = `QUESTION ${round + 1} - One group’s _ is another group’s treasure.`;
+      break;
+    case 3:
+      title.textContent = `After you have completed the group challenges to your satisfaction, enter the group word here:`;
+      break;
+    default:
+      title.textContent = `Uh oh! Something's gone wrong!`;
+  }
+  
 
   let guess = document.createElement('input');
   guess.classList.add('guess');
